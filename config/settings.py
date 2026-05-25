a4c22148e1790c722f4a3111caec84518def9bd2
Yakssha11 <ryankayle.reyes@gmail.com>
2026-05-25 20:29:18 +0800

    Add STATIC_ROOT for PythonAnywhere

--- config/settings.py
+++ config/settings.py
@@ -155,6 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/home/3csfarm/3cs_erp_web/staticfiles'
 
 LOGIN_URL          = '/login/'
 LOGIN_REDIRECT_URL = '/'
